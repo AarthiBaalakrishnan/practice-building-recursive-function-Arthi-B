569d95e6e70443d9be13b39d01890227b6752de9
AarthiBaalakrishnan <150874415+AarthiBaalakrishnan@users.noreply.github.com>
2026-06-25 13:12:50 -0400

    Add files via upload

---
+++ recursive-function.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+    if (str.length === 0) {
+        return str;
+    }
+    return str[str.length - 1] + reverseString(str.slice(0,-1));
+}
+console.log(reverseString("hello"));
+console.log(reverseString("recursion"));
+console.log(reverseString("a"));
+console.log(reverseString(""));
